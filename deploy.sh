bc07858b309e92695843d69e52e36725d55941e5
DylanChi <dylanchi@cw.com.tw>
2018-12-20 17:30:57 +0800

    wtf

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-# raw update
-msg="updated at `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git add .
-git commit -m "$msg"
 
-git push origin master
 
 # Go To Public folder
 cd public
@@ -22,7 +14,17 @@ git add .
 git commit -m "rebuilding site `date`"
 
 # Push source and build repos.
-git push origin master
+# git push origin master
 
 # Come Back up to the Project Root
 cd ..
+# raw update
+
+msg="updated at `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git add .
+git commit -m "$msg"
+
+git push origin master
