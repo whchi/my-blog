a8abf6e222fa9d5d20b3a974c17f444636b5a375
DylanChi <dylanchi@cw.com.tw>
2018-12-20 17:01:18 +0800

    updated at 2018年12月20日 週四 17時01分18秒 CST

--- deploy.sh
+++ deploy.sh
@@ -11,14 +11,21 @@ cd public
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
+git commit -m "rebuilding site `date`"
 
 # Push source and build repos.
-git push origin master
+#git push origin master
 
 # Come Back up to the Project Root
 cd ..
+
+# push using submodule
+git subtree push --prefix=public sub_public master
+
+# raw update
+msg="updated at `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -am "$msg"
+git push origin master
