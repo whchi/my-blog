fdd95119d5305abf04cc465be7079397dcdf5614
whchi <whccchi@gmail.com>
2019-06-02 21:11:15 +0800

    updated at 2019年 6月 2日 週日 21時11分15秒 CST

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# pull remote update
+git submodule update
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
