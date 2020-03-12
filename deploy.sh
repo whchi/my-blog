cfe2293178f01452e5136b2a3cb81f1558cc90d6
DylanChi <dylanchi@cw.com.tw>
2020-03-12 17:11:53 +0800

    updated at Thu Mar 12 17:11:53 CST 2020

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Pull latest change
+cd public && git pull
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
