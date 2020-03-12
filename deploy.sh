be3ce0904f08e77bad6e88c9c15d8cd7e331d935
DylanChi <dylanchi@cw.com.tw>
2020-03-12 17:12:37 +0800

    updated at Thu Mar 12 17:12:37 CST 2020

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m";
 
 # Pull latest change
-cd public && git pull
+cd public && git pull && cd -
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
